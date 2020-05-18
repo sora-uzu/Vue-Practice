f797aeb5140eeb28ad618eead03728b20a3a697e
sora-uzu <naruto07170717@gmail.com>
2020-05-19 00:05:23 +0900

    [Update]todo function

--- js/main.js
+++ js/main.js
@@ -2,13 +2,34 @@
   'use strict';
 
   var vm = new Vue({
-  	el: '#app',
-  	data: {
-  		todos: [
-  		 'task 1',
-  		 'task 2',
-  		 'task 3'
-  		]
-  	}
+    el: '#app',
+    data: {
+      newItem: '',
+      todos: [{
+        title: 'task 1',
+        isDone: false
+      }, {
+        title: 'task 2',
+        isDone: false
+      }, {
+        title: 'task 3',
+        isDone: true
+      }]
+    },
+    methods: {
+      addItem: function() {
+        var item = {
+          title: this.newItem,
+          isDone: false
+        };
+        this.todos.push(item);
+        this.newItem = '';
+      },
+      deleteItem: function(index) {
+        if (confirm('are you sure?')) {
+          this.todos.splice(index, 1);
+        }
+      }
+    }
   });
 })();
