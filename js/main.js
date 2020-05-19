f7b8d3f3a6ed09fe8b8bef0fda5502285d95745a
sora-uzu <naruto07170717@gmail.com>
2020-05-19 17:18:51 +0900

    [Finish]todo function

--- js/main.js
+++ js/main.js
@@ -5,16 +5,18 @@
     el: '#app',
     data: {
       newItem: '',
-      todos: [{
-        title: 'task 1',
-        isDone: false
-      }, {
-        title: 'task 2',
-        isDone: false
-      }, {
-        title: 'task 3',
-        isDone: true
-      }]
+      todos: []
+    },
+    watch: {
+    	todos: {
+  			handler: function() {
+    		localStorage.setItem('todos', JSON.stringify(this.todos));
+    	},
+    	deep: true
+       }
+    },
+    mounted: function() {
+    	this.todos = JSON.parse(localStorage.getItem('todos')) || [];
     },
     methods: {
       addItem: function() {
@@ -29,14 +31,19 @@
         if (confirm('are you sure?')) {
           this.todos.splice(index, 1);
         }
-      }
+      },
+      purge: function() {
+        if (!confirm('delete finished?')) {
+          return;
+        }
+        this.todos = this.remaining;
+     }
     },
     computed: {
     	remaining: function() {
-    		var items = this.todos.filter(function(todo) {
+    		return this.todos.filter(function(todo) {
     			return !todo.isDone;
     		});
-    		return items.length;
     	}
     }
   });
